c18efa1e80aa1910b0be56bbb5574d412eee0173
Yuborama <davidjohan2@hotmail.com>
2026-07-08 05:48:48 -0500

    style: add submenu solutions

--- wp-content/themes/gya/header.php
+++ wp-content/themes/gya/header.php
@@ -11,6 +11,18 @@
 $front_page_id = (int) get_option('page_on_front');
 $header_cta_text = gya_get_field_value('gya_header_cta_text', 'Diagnóstico estratégico', $front_page_id);
 $header_cta_url = gya_get_field_value('gya_header_cta_url', '#contacto', $front_page_id);
+$header_solution_items = get_posts(
+    array(
+        'post_type' => 'gya_category',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'orderby' => array(
+            'menu_order' => 'ASC',
+            'date' => 'ASC',
+        ),
+    )
+);
+$header_is_insights_active = is_post_type_archive('insights') || is_singular('insights');
 ?>
 <header class="site-header" id="top">
     <div class="shell header-inner">
@@ -19,16 +31,34 @@ $header_cta_url = gya_get_field_value('gya_header_cta_url', '#contacto', $front_
         </a>
 
         <nav class="desktop-nav" aria-label="Principal">
-            <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'primary',
-                    'container' => false,
-                    'menu_class' => 'menu',
-                    'fallback_cb' => 'gya_primary_menu_fallback',
-                )
-            );
-            ?>
+            <ul class="header-menu">
+                <li class="header-menu-item header-menu-item--dropdown">
+                    <a class="header-menu-link" href="<?php echo esc_url(home_url('/#soluciones')); ?>" aria-haspopup="true">
+                        Soluciones
+                        <span class="header-menu-chevron" aria-hidden="true"></span>
+                    </a>
+
+                    <?php if (!empty($header_solution_items)) : ?>
+                        <div class="header-dropdown">
+                            <?php foreach ($header_solution_items as $solution_item) : ?>
+                                <a href="<?php echo esc_url(get_permalink($solution_item->ID)); ?>">
+                                    <?php echo esc_html(get_the_title($solution_item)); ?>
+                                    <span aria-hidden="true">›</span>
+                                </a>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                </li>
+                <li class="header-menu-item">
+                    <a class="header-menu-link <?php echo $header_is_insights_active ? 'is-active' : ''; ?>" href="<?php echo esc_url(get_post_type_archive_link('insights') ?: home_url('/insights/')); ?>">Insights</a>
+                </li>
+                <li class="header-menu-item">
+                    <a class="header-menu-link" href="<?php echo esc_url(home_url('/#servicios')); ?>">Cómo ayudamos</a>
+                </li>
+                <li class="header-menu-item">
+                    <a class="header-menu-link" href="<?php echo esc_url(home_url('/#nosotros')); ?>">Nosotros</a>
+                </li>
+            </ul>
         </nav>
 
         <div class="header-tools">
