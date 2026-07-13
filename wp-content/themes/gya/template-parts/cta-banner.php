ef8980a2064878965bddbf96e4eb26c5641d2629
Yuborama <davidjohan2@hotmail.com>
2026-07-13 08:10:11 -0500

    feat: actualizar estilos del banner y cambiar la imagen de fondo en la sección de CTA

--- wp-content/themes/gya/template-parts/cta-banner.php
+++ wp-content/themes/gya/template-parts/cta-banner.php
@@ -9,21 +9,19 @@ $page_id = isset($args['page_id']) ? (int) $args['page_id'] : get_queried_object
 $cta_title = gya_get_field_value('gya_cta_title', 'Más que servicios, construimos relaciones.', $page_id);
 $cta_body = gya_get_field_value('gya_cta_body', 'G&A es una firma boutique que acompaña a empresas con claridad, experiencia y atención personalizada.', $page_id);
 $cta_link_text = gya_get_field_value('gya_cta_link_text', 'Hablemos de tu empresa', $page_id);
-$cta_link_url = gya_get_field_value('gya_cta_link_url', '#contacto', $page_id);
+$cta_link_url = gya_get_field_value('gya_cta_link_url', '/contact', $page_id);
 $upload_dir = wp_upload_dir();
-$banner_bg = trailingslashit($upload_dir['baseurl']) . '2026/07/network-bg.png';
-$cta_image = trailingslashit($upload_dir['baseurl']) . '2026/07/insightsCardTeam.jpg';
+$cta_banner_image = trailingslashit($upload_dir['baseurl']) . '2026/07/Banner-contacto.png';
 
 ?>
 <section class="cta-shell" id="nosotros">
     <div class="shell">
-        <article class="cta-banner">
-            <div class="cta-copy" style="background-image:url('<?php echo esc_url($banner_bg); ?>');">
+        <article class="cta-banner" style="background-image:url('<?php echo esc_url($cta_banner_image); ?>');">
+            <div class="cta-copy">
                 <h2><?php echo esc_html($cta_title); ?></h2>
                 <p><?php echo esc_html($cta_body); ?></p>
                 <a class="outline-link" href="<?php echo esc_url($cta_link_url); ?>"><?php echo esc_html($cta_link_text); ?> <span>&rsaquo;</span></a>
             </div>
-            <div class="cta-photo" style="background-image:url('<?php echo esc_url($cta_image); ?>');"></div>
         </article>
     </div>
 </section>
